669c36639be8e65a7995c6084d12c6619f521b3e
CesarQuintero <cesar@bego.ai>
2024-06-05 15:30:34 -0600

    Basic Structure and tests of Gsap integration

---
+++ app/utils/components/gsapExample.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRef } from "react";
+import styles from "../css/gsapExample.module.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+export default function GsapExample() {
+  const container = useRef<HTMLDivElement>(null);
+  const circle = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      // use selectors...
+      gsap.to(`.${styles.box}`, { rotation: "+=360", duration: 3 });
+
+      // or refs...
+      gsap.to(circle.current, { rotation: "-=360", duration: 3 });
+    },
+    { scope: container }
+  ); // <-- scope for selector text (optional)
+
+  return (
+    <div className={styles.main}>
+      <div ref={container} className={`${styles.container}`}>
+        <div className={`${styles.box} ${styles.gradientBlue}`}>selector</div>
+        <div
+          className={`${styles.circle} ${styles.gradientGreen}`}
+          ref={circle}
+        >
+          Ref
+        </div>
+      </div>
+      <div className={`${styles.box} ${styles.gradientBlue}`}>selector</div>
+    </div>
+  );
+}
---
+++ app/utils/components/gsapInteraction.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import styles from "../css/gsapExample.module.css";
+
+type Props = {};
+
+const GsapInteraction = (props: Props) => {
+  const container = useRef<HTMLDivElement>(null);
+
+  const { contextSafe } = useGSAP({ scope: container });
+
+  const onClickGood = contextSafe(() => {
+    gsap.to(`.${styles.good}`, { rotation: "+=180" });
+  });
+
+  return (
+    <div className={styles.main}>
+      <div ref={container} className={styles.container}>
+        <button onClick={onClickGood} className={`${styles.good}`}>
+          Hellow
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default GsapInteraction;
