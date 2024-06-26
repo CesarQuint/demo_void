84023251465723679b2bc50b44cc3108d4240b9b
CesarQuint <pedrocesarquintero001@gmail.com>
2024-06-26 11:45:08 -0600

    Footer improvement

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import styles from "../css/footer.module.css";
 import Title from "./title";
+import Link from "next/link";
 
 type Props = {};
 
@@ -12,21 +13,50 @@ const Footer = (props: Props) => {
         <motion.section style={{ width: "100%" }}>
           <Title
             text="VOIDXR"
-            words={8}
+            words={12}
           />
         </motion.section>
 
-        <motion.section className={`${styles.questions}`}>
-          <h4>¿Tienes una idea?</h4>
-          <p>Cuentanos</p>
-          <button>Let's Talk</button>
+        <motion.section className={` ${styles.text_container}`}>
+          <motion.div className={`${styles.flex_container}  ${styles.concat}`}>
+            <div className={`${styles.contact_button_area_wrapper}`}>
+              <p>¿Quieres empezar un nuevo proyecto?</p>
+              <section className={`${styles.about_buttons}`}>
+                <button className={`${styles.btn_1}`}>ESCRIBENOS</button>
+                <button className={`${styles.btn_2}`}>+</button>
+              </section>
+            </div>
+            <p>CONTACTO@VOIDXR.STUDIO</p>
+          </motion.div>
+          <motion.div
+            className={`${styles.flex_container}  ${styles.information}`}>
+            <p>17:31:51</p>
+            <p>
+              Watteau 35, 03700
+              <br /> Ciudad de México,
+              <br /> CDMX
+            </p>
+            <p>Void XR 2024®</p>
+          </motion.div>
+          <motion.div className={`${styles.flex_container}  ${styles.nav}`}>
+            <p>NAVIGATION</p>
+            <nav className={`${styles.links}`}>
+              <Link href={"/"}>HOME</Link>
+              <Link href={"/"}>PROJECT</Link>
+              <Link href={"/"}>STUDIO</Link>
+              <Link href={"/"}>CONTACT</Link>
+            </nav>
+          </motion.div>
+          <motion.div className={`${styles.flex_container}  ${styles.social}`}>
+            <p>SOCIAL</p>
+            <nav className={`${styles.links}`}>
+              <Link href={"/"}>INSTAGRAM</Link>
+              <Link href={"/"}>FACEBOOK</Link>
+              <Link href={"/"}>VIDEO</Link>
+              <Link href={"/"}>LINKEDIN</Link>
+            </nav>
+          </motion.div>
         </motion.section>
-        <motion.section className={`${styles.social}`}>
-          <p>Instagram</p>
-          <p>Facebook</p>
-          <p>Vimoe</p>
-        </motion.section>
-        <motion.section>Void XR</motion.section>
       </motion.section>
     </motion.div>
   );
