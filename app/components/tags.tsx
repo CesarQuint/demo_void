3e53edc7ffc3de6da5b63b1a8980a8c60c8b106e
Axel Vasquez <axel@bego.ai>
2024-07-09 01:45:44 -0600

    updating tags scroll logic

--- app/components/tags.tsx
+++ app/components/tags.tsx
@@ -1,102 +1,87 @@
-import React, { useRef } from "react";
-import styles from "../css/tags.module.css";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { motion } from "framer-motion";
-import { useGSAP } from "@gsap/react";
-import { AboutUsTag } from "../constants/tags_text";
+import React, { useRef } from 'react'
+import styles from '../css/tags.module.css'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { motion } from 'framer-motion'
+import { useGSAP } from '@gsap/react'
+import { AboutUsTag } from '../constants/tags_text'
 
-gsap.registerPlugin(useGSAP, ScrollTrigger);
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 type Props = {
-  contentArr: AboutUsTag[];
-};
+  contentArr: AboutUsTag[]
+}
 
 const Tags = (props: Props) => {
-  const container = useRef<HTMLElement>(null);
+  const container = useRef<HTMLElement>(null)
 
   useGSAP(
     () => {
-      const GAP = 32;
-      const cardMargin = 50;
+      const tags = gsap.utils.toArray<HTMLElement>(`.${styles.tag}`)
+      const space = gsap.getProperty(container.current, 'gap') as number
 
-      const btn = container.current!.querySelector(
-        `.${styles.know_more}`
-      )! as HTMLElement;
-      const firstCardHeight =
-        container.current!.firstElementChild!.getBoundingClientRect().height;
-      const tags = gsap.utils.toArray(`.${styles.tag}`);
+      function setMarginTop(el: HTMLElement, i: number) {
+        const iSpace = space * (i + 1)
+        const differenceHeight = tags[0].offsetHeight - el.offsetHeight
 
-      function startAnimation() {
-        gsap.set(tags, {
-          bottom: (i, _, arr) =>
-            btn!.offsetHeight + GAP + cardMargin * (arr.length - i - 1),
-          zIndex: (i, _, arr) => arr.length - i,
-        });
-
-        gsap
-          .timeline({
-            defaults: {
-              ease: "none",
-            },
-            scrollTrigger: {
-              trigger: btn,
-              start: "bottom bottom-=5%",
-              end: "bottom bottom-=5%",
-              scrub: true,
-              invalidateOnRefresh: true,
-            },
-          })
-          .fromTo(
-            btn,
-            {
-              marginTop: firstCardHeight + GAP + cardMargin * (tags.length - 1),
-            },
-            { marginTop: 0 }
-          )
-          .fromTo(tags, { marginTop: (i) => cardMargin * i }, { marginTop: 0 });
-
-        gsap
-          .timeline({
-            defaults: {
-              ease: "none",
-            },
-            scrollTrigger: {
-              trigger: container.current,
-              start: "top center+=10%",
-              end: `${firstCardHeight} center`,
-              scrub: true,
-            },
-          })
-          .fromTo(tags, { scale: (i) => 1 - i * 0.05 }, { scale: 1 }, 0);
+        gsap.set(el, { marginTop: differenceHeight + iSpace })
       }
 
-      const ob = new IntersectionObserver(
-        ([ev]) => {
-          if (ev.isIntersecting || ev.boundingClientRect.top < 0) {
-            startAnimation();
-            ob.disconnect();
-          }
-        },
-        { root: null, rootMargin: "0px", threshold: 0 }
-      );
+      tags.slice(1).forEach((el, i, arr) => {
+        const iSpace = i === 0 ? space : 0
+        const start = tags[i].offsetTop + tags[i].clientHeight + iSpace
+        const scrollStart = space * (arr.length - i)
 
-      ob.observe(container.current!);
-      return () => ob.disconnect();
+        gsap.fromTo(
+          el,
+          { scale: 0.95 - i * 0.02 },
+          {
+            scale: 1,
+            ease: 'none',
+            scrollTrigger: {
+              trigger: container.current,
+              start: `${start} bottom-=${scrollStart}`,
+              end: `+=50 bottom-=${scrollStart}`,
+              scrub: true,
+            },
+            onStart() {
+              gsap.set(el, { marginTop: 'auto' })
+            },
+            onReverseComplete() {
+              setMarginTop(el, i)
+            },
+          },
+        )
+      })
+
+      gsap.set(tags, {
+        bottom: (i) => space * (tags.length - i),
+        zIndex: (i) => tags.length - i,
+        position: 'sticky',
+      })
+
+      const ob = new IntersectionObserver(([ev]) => {
+        if (!ev.isIntersecting) return
+
+        if (ev.boundingClientRect.top >= 0) {
+          tags.slice(1).forEach((el, i) => setMarginTop(el, i))
+        }
+
+        ob.disconnect()
+      })
+
+      ob.observe(container.current!)
+      return () => ob.disconnect()
     },
-    { scope: container }
-  );
+    { scope: container },
+  )
 
   return (
     <motion.div className={`${styles.main}`}>
       <div className={`${styles.tags_wrapper}`}>
-        <section
-          className={`${styles.tags_container}`}
-          ref={container}>
+        <section className={`${styles.tags_container}`} ref={container}>
           {props.contentArr.map((_, i) => (
-            <div
-              key={i}
-              className={`${styles.tag}`}>
+            <div key={i} className={`${styles.tag}`}>
               <section className={styles.left_content}>
                 <h4>{_.number}</h4>
               </section>
@@ -113,11 +98,10 @@ const Tags = (props: Props) => {
               </article>
             </div>
           ))}
-          <button className={`${styles.know_more}`}>Ver Todos</button>
         </section>
       </div>
     </motion.div>
-  );
-};
+  )
+}
 
-export default Tags;
+export default Tags
