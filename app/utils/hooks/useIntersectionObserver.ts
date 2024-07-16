5124f7b054ca7408bda4d175b7f5c55f8d76727f
CesarQuint <pedrocesarquintero001@gmail.com>
2024-07-16 06:56:28 -0600

    Observer hook

---
+++ app/utils/hooks/useIntersectionObserver.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, useRef } from "react";
+
+export function useIntersectionObserver() {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin: "200px",
+      }
+    );
+
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return { isIntersecting, ref };
+}
