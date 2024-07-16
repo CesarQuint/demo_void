1132350ce862129d6860b41a7238f403bfbe89cd
CesarQuint <pedrocesarquintero001@gmail.com>
2024-07-16 06:55:31 -0600

    framer main restructure

--- app/frammer_main/page.tsx
+++ app/frammer_main/page.tsx
@@ -10,15 +10,13 @@ import { animate as animation } from "framer-motion";
 
 import Footer from "../components/footer";
 import HeroContainer from "../components/Home/HeroContainer";
-import VideoHover from "../components/videoHover";
-import Video from "../components/Three/Video";
 
-import RepeatTextScrollFx from "../components/gsapComponents";
 import ProjectImages from "../components/Home/ProjectImages";
-import { DisplacementGeometry } from "../hero_demo/ColumnDisplacementMaterial";
-import { Canvas } from "@react-three/fiber";
 import ProcessHome from "../components/Home/ProcessHome";
 import { PreFooterLink } from "../components/PreFooterLink";
+import Video from "../components/Three/Video";
+import Tags from "../components/tags";
+import { about_us_tags } from "../constants/tags_text";
 
 const transitionSpringPhysics: Spring = {
   type: "spring",
@@ -113,18 +111,7 @@ function SecondPage() {
         transition={transitionSpringPhysics}
         className="courtain"
       />
-      <section>
-        <Canvas style={{ height: "100vh" }}>
-          <DisplacementGeometry settings={{
-            grid: true,
-            glow: 0.15,
-            columns: 10,
-            orb_size: 0.5,
-            contrast: 5.0,
-            easing_factor: 0.05,
-          }} />
-        </Canvas>
-      </section>
+      <HeroContainer />
       {/* <Video /> */}
       <ProjectImages />
       <ProcessHome />
