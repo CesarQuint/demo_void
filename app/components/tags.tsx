82eb35b22837f407f8f37d1b853eedbe0df60ad8
CesarQuint <pedrocesarquintero001@gmail.com>
2024-07-16 01:31:23 -0600

    Refactoring the component

--- app/components/tags.tsx
+++ app/components/tags.tsx
@@ -1,4 +1,5 @@
-import React, { useRef } from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import styles from "../css/tags.module.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -23,122 +24,135 @@ type TagsContentProps = {
 const TagsContent = (props: TagsContentProps) => {
   const windowStatus = useWindow();
 
-  return (
-    <div key={props.i} className={`${styles.tag}`}>
-      {windowStatus.innerHeight < 768 ? (
-        <>
+  if (windowStatus && windowStatus.innerWidth < 768) {
+    return (
+      <>
+        <section>
+          <p className={styles.right_numeral}>{props.number}</p>
+          <h5 className={styles.title}>{props.title}</h5>
+        </section>
+        <section className={styles.left_content}>
+          <h4>{props.number}</h4>
+        </section>
+        <div className={styles.right_text_content}>
+          {props.content.map((text, i) => (
+            <p key={i}>{text}</p>
+          ))}
+        </div>
+      </>
+    );
+  }
+
+  if (windowStatus && windowStatus.innerWidth > 768) {
+    return (
+      <>
+        <section className={styles.left_content}>
+          <h4>{props.number}</h4>
+        </section>
+        <article className={styles.right_content}>
           <section>
             <p className={styles.right_numeral}>{props.number}</p>
             <h5 className={styles.title}>{props.title}</h5>
-          </section>
-          <section className={styles.left_content}>
-            <h4>{props.number}</h4>
           </section>
           <div className={styles.right_text_content}>
             {props.content.map((text, i) => (
               <p key={i}>{text}</p>
             ))}
           </div>
-        </>
-      ) : (
-        <>
-          <section className={styles.left_content}>
-            <h4>{props.number}</h4>
-          </section>
-          <article className={styles.right_content}>
-            <section>
-              <p className={styles.right_numeral}>{props.number}</p>
-              <h5 className={styles.title}>{props.title}</h5>
-            </section>
-            <div className={styles.right_text_content}>
-              {props.content.map((text, i) => (
-                <p key={i}>{text}</p>
-              ))}
-            </div>
-          </article>
-        </>
-      )}
-    </div>
-  );
+        </article>
+      </>
+    );
+  }
 };
 
 const Tags = (props: Props) => {
-  const container = useRef<HTMLElement>(null);
+  const container = useRef<HTMLDivElement>(null);
+  const windowStatus = useWindow();
 
   useGSAP(
     () => {
-      const tags = gsap.utils.toArray<HTMLElement>(`.${styles.tag}`);
-      const space = gsap.getProperty(container.current, "gap") as number;
+      if (container.current !== null) {
+        const tags = gsap.utils.toArray<HTMLDivElement>(`.${styles.tag}`);
+        const space = gsap.getProperty(container.current, "gap") as number;
 
-      const margins = tags.slice(1).map((el, i) => {
-        const iSpace = space * (i + 1);
-        const differenceHeight = tags[0].offsetHeight - el.offsetHeight;
+        const margins = tags.slice(1).map((el, i) => {
+          const iSpace = space * (i + 1);
+          const differenceHeight = tags[0].offsetHeight - el.offsetHeight;
 
-        return differenceHeight + iSpace;
-      });
+          return differenceHeight + iSpace;
+        });
 
-      tags.slice(1).forEach((el, i, arr) => {
-        const start = tags[i].offsetTop + tags[i].clientHeight;
-        const end = tags[i + 1].offsetTop + tags[i + 1].offsetHeight;
-        const scrollStart = space * arr.length;
+        tags.slice(1).forEach((el, i, arr) => {
+          const start = tags[i].offsetTop + tags[i].clientHeight;
+          const end = tags[i + 1].offsetTop + tags[i + 1].offsetHeight;
+          const scrollStart = space * arr.length;
+          console.log(start);
 
-        gsap.fromTo(
-          el,
-          { scale: 0.95 - i * 0.02 },
-          {
-            scale: 1,
-            ease: "none",
-            scrollTrigger: {
-              trigger: container.current,
-              start: `${start} bottom-=${scrollStart}`,
-              end: `${end} bottom-=${scrollStart}`,
-              scrub: true,
-              markers: true,
-              onUpdate(e) {
-                const iSpace = space * (i + 1);
-                const differenceHeight =
-                  tags[0].offsetHeight - el.offsetHeight + iSpace;
-                const y = (e.end - e.start) * e.progress;
+          gsap.fromTo(
+            el,
+            { scale: 0.95 - i * 0.02 },
+            {
+              scale: 1,
+              ease: "none",
+              scrollTrigger: {
+                trigger: container.current,
+                start: `${start} bottom-=${scrollStart}`,
+                end: `${end} bottom-=${scrollStart}`,
+                markers: true,
+                scrub: true,
+                onUpdate(e) {
+                  const iSpace = space * (i + 1);
+                  const differenceHeight =
+                    tags[0].offsetHeight - el.offsetHeight + iSpace;
+                  const y = (e.end - e.start) * e.progress;
 
-                gsap.set(el, {
-                  marginTop: y > differenceHeight ? "auto" : margins[i],
-                });
+                  gsap.set(el, {
+                    marginTop: y > differenceHeight ? "auto" : margins[i],
+                  });
+                },
               },
-            },
-          },
-        );
-      });
+            }
+          );
+        });
 
-      gsap.set(container.current, {
-        maxHeight: container.current!.scrollHeight,
-      });
+        gsap.set(container.current, {
+          maxHeight: container.current!.scrollHeight,
+        });
 
-      gsap.set(tags, {
-        bottom: (i) => space * (tags.length - i),
-        zIndex: (i) => tags.length - i,
-        position: "sticky",
-      });
+        gsap.set(tags, {
+          bottom: (i) => space * (tags.length - i),
+          zIndex: (i) => tags.length - i,
+          position: "sticky",
+        });
 
-      tags.slice(1).forEach((el, i) => gsap.set(el, { marginTop: margins[i] }));
+        tags
+          .slice(1)
+          .forEach((el, i) => gsap.set(el, { marginTop: margins[i] }));
+      }
     },
-    { scope: container},
+    { scope: container, dependencies: [container] }
   );
 
   return (
     <motion.div className={`${styles.main}`}>
-      <div className={`${styles.tags_wrapper}`}>
-        <section className={`${styles.tags_container}`} ref={container}>
+      <motion.div className={`${styles.tags_wrapper}`}>
+        <motion.section
+          className={`${styles.tags_container}`}
+          ref={container}>
           {props.contentArr.map((_, i) => (
-            <TagsContent
+            <motion.div
               key={i}
-              i={i}
-              title={_.title}
-              number={_.number}
-              content={_.content}
-            />
+              className={`${styles.tag}`}>
+              <TagsContent
+                i={i}
+                title={_.title}
+                number={_.number}
+                content={_.content}
+              />
+            </motion.div>
           ))}
-        </section>
-      </div>
+        </motion.section>
+      </motion.div>
     </motion.div>
   );
 };
