d3e0707d6ba7163e226c79ff4b19f6a703c75e68
CesarQuint <pedrocesarquintero001@gmail.com>
2024-07-16 01:28:19 -0600

    Process Home left with only current values

--- app/components/Home/ProcessHome.tsx
+++ app/components/Home/ProcessHome.tsx
@@ -1,37 +1,27 @@
-"use client";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Tags from "../tags";
 import { about_us_tags } from "@/app/constants/tags_text";
-import styles from "../../css/About/process.module.css";
+import styles from "../../css/home/processHome.module.css";
 import Image from "next/image";
-import gift from "../../../public/gifts/void_gif.gif";
 
 type Props = {};
 
 const ProcessHome = (props: Props) => {
+  const [imgLoaded, setImgLoaded] = useState(false);
+
+  useEffect(() => {}, [imgLoaded]);
   return (
     <motion.section className={styles.main}>
       <h2 className={styles.title}>NUESTRO PROCESO</h2>
-      <motion.div className={styles.gif_wrapper}>
-        <Image
-          width={1000}
-          height={1000}
-          className={styles.image_gif}
-          src={gift}
-          alt="Gif"
-        />
-      </motion.div>
-
-      <section className={styles.description}>
+      <motion.div className={styles.description}>
         <p>
           Somos un grupo de artistas digitales apasionados por la tecnología y
           la comunicación, expertos en crear experiencias para solucionar los
           retos que enfrentan nuestros clientes y comunicar nuestras propias
           ideas.
         </p>
-      </section>
-
-      <Tags contentArr={about_us_tags} />
+      </motion.div>
     </motion.section>
   );
 };
