7f2db990730db82463e6cba9d058484ecae5d6a7
Omar Gallegos <omar.gallegos@thinkthrough.studio>
2024-08-19 17:47:36 -0600

    refactor: remove 'attributes' indented property
    Attributes is a property of Product rather than ImageFormat

--- app/Strapi/interfaces/Entities/ImageFormat.ts
+++ app/Strapi/interfaces/Entities/ImageFormat.ts
@@ -14,27 +14,25 @@ interface Format {
 }
 
 export interface ImageFormat extends Entity {
-  attributes: {
-    name: string;
-    alternativeText: string | null;
-    caption: string | null;
-    width: number;
-    height: number;
-    formats: {
-      small: Format;
-      medium: Format;
-      large: Format;
-      thumbnail: Format;
-    };
-    hash: string;
-    ext: string;
-    mime: string;
-    size: number;
-    url: string;
-    previewUrl: string | null;
-    provider: string;
-    provider_metadata: null;
-    createdAt: string;
-    updatedAt: string;
-  }
+  name: string;
+  alternativeText: string | null;
+  caption: string | null;
+  width: number;
+  height: number;
+  formats: {
+    small: Format;
+    medium: Format;
+    large: Format;
+    thumbnail: Format;
+  };
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: string | null;
+  provider: string;
+  provider_metadata: null;
+  createdAt: string;
+  updatedAt: string;
 }
