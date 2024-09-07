003a4b7357dfea193371762ff24a70d0aee52141
Omar Gallegos <omar.gallegos@thinkthrough.studio>
2024-09-07 14:37:24 -0600

    feat: add Content scaffolding and ContentData type

---
+++ app/components/case-study/section-components/Content.tsx
@@ -0,0 +1,26 @@
+import { CodeProps } from "../content-components/content-types/Code";
+import { ParagraphProps } from "../content-components/content-types/Paragraph";
+import { ImageProps } from "../content-components/content-types/Image";
+import { HeadingProps } from "../content-components/content-types/Heading";
+import { ListProps } from "../content-components/content-types/List";
+import styles from '../../../css/case-study/section-components.module.css'
+
+export type ContentData =
+    Array<
+        CodeProps['data'] |
+        ParagraphProps['data'] |
+        ImageProps['data'] |
+        HeadingProps['data'] |
+        ListProps['data']
+    >;
+
+export type ContentProps = { data: ContentData };
+
+const Content = ({ data }: ContentProps): React.JSX.Element =>
+(
+    <section className={styles.content}>
+        {}
+    </section>
+);
+
+export default Content;
