252d1d96ae0e92fb75aa658038279bb3c314156d
Omar Gallegos <omar.gallegos@thinkthrough.studio>
2024-09-07 14:35:56 -0600

    refactor: reduce Projects attributes and abstract content-related data

--- app/Strapi/interfaces/Entities/Project.ts
+++ app/Strapi/interfaces/Entities/Project.ts
@@ -1,29 +1,32 @@
 import { Entity } from "./Entity";
 import { Category } from "./Category"
-import { ImageFormat } from "./ImageFormat";
+import { ImageContent } from "./ImageFormat";
 
-export type TextBlock = { type: string, text: string };
-export type SectionBlock = { type: string, children: TextBlock[] };
-export type HeadingProps = SectionBlock & { level: number };
-export type Content = SectionBlock & HeadingProps & { image: ImageFormat };
+import { ContentData } from "@/app/components/case-study/section-components/Content";
+
+export enum ContentSection {
+    INTRODUCTION = 'Introduction',
+    CONCEPTUALIZATION = 'Conceptualization',
+    DEVELOPMENT = 'Development',
+    PRODUCTION = 'Production',
+    RESULTS = 'Results',
+}
 
 export interface Project extends Entity {
-  attributes: {
-    Title: string;
-    slug: string;
-    EventDate: string;
-    Subtitle: string;
-    Cover: { data: { id: number, attributes: ImageFormat } };
-    Category: { data: Category };
-    Location: string;
-    Introduction: Content[];
-    Conceptualization: Content[];
-    Development: Content[];
-    Production: Content[];
-    Videomapping: Content[];
-    Illumination: Content[];
-    Animation: Content[];
-    Interactivity: Content[];
-    Credits: Content[];
-  };
+    attributes: {
+        Title: string;
+        slug: string;
+        EventDate: string;
+        Subtitle: string;
+        Cover: { data: { id: number, attributes: ImageContent } };
+        Category: { data: Category };
+        Location: string;
+
+        [ContentSection.INTRODUCTION]: string;
+        [ContentSection.CONCEPTUALIZATION]: ContentData[] | null;
+        [ContentSection.DEVELOPMENT]: ContentData[] | null;
+        [ContentSection.PRODUCTION]: ContentData[] | null;
+        [ContentSection.RESULTS]: ContentData[] | null;
+        Credits: ContentData[] | null;
+    };
 };
