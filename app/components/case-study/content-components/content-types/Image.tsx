48a616d9801832dfa94ea8ef29fa4d59d29d3a67
Omar Gallegos <omar.gallegos@thinkthrough.studio>
2024-09-15 18:36:27 -0600

    refactor(case-study-content): add image content css module

--- app/components/case-study/content-components/content-types/Image.tsx
+++ app/components/case-study/content-components/content-types/Image.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { ImageContent } from "@/app/Strapi/interfaces/Entities/ImageFormat";
-import styles from '../../../../css/case-study/content-components.module.css'
+import styles from './Image.module.css';
 
 type ImageText = {
     type: 'text';
@@ -15,12 +15,19 @@ type ImageData = {
 
 export type ImageProps = { data: ImageData };
 
+const chooseFormat = (data: ImageData['image']) =>
+    data.formats.large ??
+    data.formats.medium ??
+    data.formats.small ??
+    data.formats.thumbnail;
+
 const CaseStudyImage = ({ data }: ImageProps): React.JSX.Element =>
 (
-    <div className={styles.case_study_img}>
-        <figure>
+    <div className={styles.imageContainer}>
+        <figure className={styles.imageFigure}>
             <Image
-                src={process.env.NEXT_PUBLIC_STRAPI_BASE_URL + data.image.url}
+                className={styles.imageImg}
+                src={process.env.NEXT_PUBLIC_STRAPI_BASE_URL + chooseFormat(data.image).url}
                 alt={data.image.alternativeText || 'Image'}
                 width={data.image.width}
                 height={data.image.height}
@@ -29,7 +36,7 @@ const CaseStudyImage = ({ data }: ImageProps): React.JSX.Element =>
                 priority
             />
             {data.image.caption &&
-                <figcaption>
+                <figcaption className={styles.imageCaption}>
                     {data.image.caption}
                 </figcaption>
             }
