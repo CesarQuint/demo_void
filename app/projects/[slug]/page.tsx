a04852185e966ea2f83317ecb8792f641ab92279
Omar Gallegos <omar.gallegos@thinkthrough.studio>
2024-09-16 21:11:29 -0600

    fix(case-study-view): filter out viewing project from related projects array

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -3,7 +3,9 @@ import Horizontal from "../../horizontal/page";
 
 export default async function ProjectView({ params }: { params: { slug: string } }) {
     const PROJECT = await getProjectDetails({ slug: params.slug });
-    const RELATED = await getProjectsByCategory({ slug: PROJECT.data[0].attributes.Category.data.attributes.slug });
+    const RELATED = await getProjectsByCategory({
+        slug: PROJECT.data[0].attributes.Category.data.attributes.slug
+    }).then((data) => data.data.filter((project) => project.id !== PROJECT.data[0].id));
 
-    return <Horizontal data={{ project: PROJECT.data[0], related: RELATED.data }} />
+    return <Horizontal data={{ project: PROJECT.data[0], related: RELATED }} />
 };
