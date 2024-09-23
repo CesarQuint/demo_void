c70022c9f4f719178dff3c088af1a82d775da351
CesarQuint <pedrocesarquintero001@gmail.com>
2024-09-22 18:30:20 -0600

    Modify router events to handle return

--- app/utils/navigationContext.tsx
+++ app/utils/navigationContext.tsx
@@ -1,7 +1,6 @@
-// NavigationContext.tsx
 "use client";
 import React, { createContext, useState, useContext, useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 interface NavigationContextProps {
   navigationEvent: { state: boolean; href: string };
@@ -18,12 +17,30 @@ export const NavigationProvider = ({
   children: React.ReactNode;
 }) => {
   const pathname = usePathname();
+  const router = useRouter(); // Access the router
   const [navigationEvent, setNavigationEvent] = useState({
     state: true,
     href: pathname,
   });
 
-  useEffect(() => {}, [navigationEvent]);
+  // Listen for popstate (back/forward navigation events)
+  useEffect(() => {
+    const handlePopState = () => {
+      const currentPath = window.location.pathname;
+      setNavigationEvent({ state: true, href: currentPath });
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Ensure that we set the current pathname when navigating manually
+    setNavigationEvent({ state: true, href: pathname });
+  }, [pathname]);
 
   return (
     <NavigationContext.Provider value={{ navigationEvent, setNavigationEvent }}>
