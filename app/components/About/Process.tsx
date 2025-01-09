db501d1a34613acb094c91f73115ebfb61b2a7cc
Omar Gallegos <omar.gallegos@thinkthrough.studio>
2025-01-09 17:06:25 -0600

    feat(about-process): integrate `AnimatedStackedCards` to about's `Process`

--- app/components/About/Process.tsx
+++ app/components/About/Process.tsx
@@ -2,14 +2,22 @@ import { motion } from "framer-motion";
 import Tags from "../tags";
 import { about_us_tags } from "../../constants/tags_text";
 import styles from "../../css/About/process.module.css";
+import AnimatedStackedCards from "../StackedCards/AnimatedStackedCards";
+import useWindow from "@/app/utils/hooks/useWindow";
 
 type Props = {};
 
 const Process = (props: Props) => {
+    const window = useWindow();
+
     return (
         <motion.section className={styles.main}>
             <h2 className={styles.title}>NUESTRO PROCESO</h2>
-            <Tags contentArr={about_us_tags} />
+            {window && window.innerWidth > 768 ? (
+                <Tags contentArr={about_us_tags} />
+            ) : (
+                <AnimatedStackedCards content={about_us_tags} />
+            )}
         </motion.section>
     );
 };
