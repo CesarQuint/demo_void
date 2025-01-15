6f001b04333d3a70a31322c4408dae9b098a0be7
Omar Gallegos <omar.gallegos@thinkthrough.studio>
2025-01-15 04:09:24 -0600

    fix(process): add `useWindow()` hook for access to window object

--- app/components/About/Process.tsx
+++ app/components/About/Process.tsx
@@ -3,10 +3,13 @@ import { about_us_tags } from "../../constants/tags_text";
 import styles from "../../css/About/process.module.css";
 import AnimatedStackedCards from "../StackedCards/AnimatedStackedCards";
 import dynamic from "next/dynamic";
+import useWindow from "@/app/utils/hooks/useWindow";
 
 const Tags = dynamic(() => import("../tags"), { ssr: false });
 
 const Process = () => {
+    const window = useWindow();
+
     return (
         <motion.section className={styles.main}>
             <h2 className={styles.title}>NUESTRO PROCESO</h2>
